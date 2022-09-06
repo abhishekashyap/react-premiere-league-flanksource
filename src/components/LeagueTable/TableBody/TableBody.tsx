12d20775a526fbabf5052bf5a6797908d5344fc1
Abhishek Kashyap <aksp72@gmail.com>
2022-09-07 02:17:31 +0530

    feat(league-table): add table body component

---
+++ src/components/LeagueTable/TableBody/TableBody.tsx
@@ -0,0 +1,27 @@
+import { FunctionComponent } from "react";
+import TableData from "./TableData";
+import TableRow from "./TableRow";
+import { TableBodyProps } from "./types";
+
+export const TableBody: FunctionComponent<TableBodyProps> = ({ dataList }) => (
+  <tbody>
+    {dataList.map(
+      ({ name, gamesPlayed, win, draw, loss, goals, points }, idx) => (
+        <TableRow key={name}>
+          <TableData>{idx + 1}</TableData>
+          <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+            {name}
+          </th>
+          <TableData>{gamesPlayed}</TableData>
+          <TableData>{win}</TableData>
+          <TableData>{draw}</TableData>
+          <TableData>{loss}</TableData>
+          <TableData>{goals?.score - goals?.concede}</TableData>
+          <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-center text-blueGray-700 ">
+            {points}
+          </th>
+        </TableRow>
+      )
+    )}
+  </tbody>
+);
---
+++ src/components/LeagueTable/TableBody/types.ts
@@ -0,0 +1,5 @@
+import { LeagueTableDataType } from "@utils/types";
+
+export type TableBodyProps = {
+  dataList: LeagueTableDataType[];
+};
