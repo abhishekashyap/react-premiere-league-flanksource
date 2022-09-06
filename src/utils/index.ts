08c21751ad3abca1c606bc18376fab1b934b0ca5
Abhishek Kashyap <aksp72@gmail.com>
2022-09-06 23:58:24 +0530

    chore(league-table): export all utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,4 @@
 export { default as getAllMatches } from "./getAllMatches";
+export { default as getTeamRecord } from "./getTeamRecord";
 export { default as getTeams } from "./getTeams";
+export { default as getTeamsData } from "./getTeamsData";
