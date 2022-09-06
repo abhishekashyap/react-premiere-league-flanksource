4db60b5232604f5d4c81f743f894253882e7c475
Abhishek Kashyap <aksp72@gmail.com>
2022-09-06 21:27:06 +0530

    feat(league-table): parse data to get matches

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { getAllMatches } from "@utils";
+import { data } from "./data";
 import "./styles.css";
 
 export default function App() {
+  const matches = getAllMatches(data);
+
   return (
     <div className="bg-slate-400">
       <h1>Hello Codesandbox</h1>
