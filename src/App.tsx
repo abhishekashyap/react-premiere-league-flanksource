c0c926666c9f4f23069e11fbecfebdc0a6901074
Abhishek Kashyap <aksp72@gmail.com>
2022-09-06 20:42:48 +0530

    chore(dev): add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="bg-slate-400">
       <h1>Hello Codesandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
