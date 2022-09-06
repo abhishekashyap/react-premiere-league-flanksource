11e53c567a83f5717ea8a370615df495cd73b685
Abhishek Kashyap <aksp72@gmail.com>
2022-09-07 02:29:14 +0530

    chore(fixture-list): export all utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 export { default as generateLeagueTableData } from "./generateLeagueTableData";
 export { default as getAllMatches } from "./getAllMatches";
+export { default as getFixtureList } from "./getFixtureList";
 export { default as getTeamRecord } from "./getTeamRecord";
 export { default as getTeams } from "./getTeams";
 export { default as getTeamsData } from "./getTeamsData";
