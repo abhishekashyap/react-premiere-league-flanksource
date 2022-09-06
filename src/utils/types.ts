391a473b533ca93c92cbfc29addd2e9c16c0b638
Abhishek Kashyap <aksp72@gmail.com>
2022-09-06 21:44:18 +0530

    feat(league-table): add types for team, record, teamList and recordList

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,27 @@
+export type TeamList = {
+  [teamName: string]: Team;
+};
+
+export type TeamRecordList = {
+  [teamName: string]: Record;
+};
+
+export type Team = Record & {
+  goals: Goal;
+};
+
+export type Record = {
+  win: number;
+  loss: number;
+  draw: number;
+  gamesPlayed: number;
+};
+
+export type Goal = {
+  concede: number;
+  score: number;
+};
+
 export type Match = {
   match: string;
   date: string;
