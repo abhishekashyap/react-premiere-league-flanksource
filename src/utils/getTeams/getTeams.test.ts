1168d023ee27dd4ac37840d491a6d69341a0b1fe
Abhishek Kashyap <aksp72@gmail.com>
2022-09-08 02:33:43 +0530

    test: add tests for getTeams util

---
+++ src/utils/getTeams/getTeams.test.ts
@@ -0,0 +1,25 @@
+import { getTeams } from "./getTeams";
+
+const mockTeamsNamesArr = ["Manchester City", "Manchester United", "Liverpool"];
+const expectedReturn = [
+  { team1: "Manchester City" },
+  { team2: "Manchester United" },
+  { team3: "Liverpool" },
+];
+describe("getTeams", () => {
+  it("should return enumerated team lists array of objects", () => {
+    expect(getTeams(mockTeamsNamesArr)).toEqual(expectedReturn);
+  });
+
+  it("should return empty array if empty array is provided as argument", () => {
+    expect(getTeams([])).toEqual([]);
+  });
+
+  it("should return empty array if null is provided as argument", () => {
+    expect(getTeams(null as any)).toEqual([]);
+  });
+
+  it("should return empty array if undefined is provided as argument", () => {
+    expect(getTeams(undefined as any)).toEqual([]);
+  });
+});
--- src/utils/getTeams/getTeams.ts
+++ src/utils/getTeams/getTeams.ts
@@ -1,4 +1,9 @@
 import { GetTeamsArgs } from "./types";
 
+/**
+ * Get enumerated list of two or more teams
+ * @param  {GetTeamsArgs} teams - Array of strings containing team names
+ * @returns Array of objects containing team iteration as key and team name as value. Returns empty list if argument is invalid.
+ */
 export const getTeams = (teams: GetTeamsArgs) =>
-  teams.map((teamName, idx) => ({ [`team${idx + 1}`]: teamName }));
+  teams ? teams.map((teamName, idx) => ({ [`team${idx + 1}`]: teamName })) : [];
