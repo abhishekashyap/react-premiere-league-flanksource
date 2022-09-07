10657f91f0de8cf2c11b717c7f02853537ea9d44
Abhishek Kashyap <aksp72@gmail.com>
2022-09-08 01:58:54 +0530

    refactor: match the expected date time format

--- src/components/FixtureList/MatchList/MatchList.tsx
+++ src/components/FixtureList/MatchList/MatchList.tsx
@@ -8,7 +8,7 @@ export const MatchList: FunctionComponent<MatchListProps> = ({ matches }) => {
       {matches.map((match) => {
         const parsedDate = parseISO(match.date);
         const formattedDate = format(parsedDate, "EEEE d MMMM y");
-        const formattedTime = format(parsedDate, "HH:mm");
+        const formattedDateTime = format(parsedDate, "dd/MM, HH:mm");
         return (
           <li key={match.match}>
             <h4 className="mt-8 text-xl font-semibold text-gray-600">
@@ -16,7 +16,7 @@ export const MatchList: FunctionComponent<MatchListProps> = ({ matches }) => {
             </h4>
             <div className="text-gray-700 mt-2 flex justify-between border border-gray-200 rounded overflow-hidden px-4 py-2 bg-white">
               <span className="flex-1 font-semibold">{match.team1}</span>
-              <span className="flex-1 text-center">{formattedTime}</span>
+              <span className="flex-1 text-center">{formattedDateTime}</span>
               <span className="flex-1 text-right font-semibold">
                 {match.team2}
               </span>
