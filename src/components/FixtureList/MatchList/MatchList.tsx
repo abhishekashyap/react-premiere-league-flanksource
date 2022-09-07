6adc25073d1481138ddfbb7d52e686fba96ab3c7
Abhishek Kashyap <aksp72@gmail.com>
2022-09-08 01:48:00 +0530

    style: remove shadows from fixtures

--- src/components/FixtureList/MatchList/MatchList.tsx
+++ src/components/FixtureList/MatchList/MatchList.tsx
@@ -14,7 +14,7 @@ export const MatchList: FunctionComponent<MatchListProps> = ({ matches }) => {
             <h4 className="mt-8 text-xl font-semibold text-gray-600">
               {formattedDate}
             </h4>
-            <div className="text-gray-700 mt-2 flex justify-between border border-gray-200 rounded overflow-hidden shadow-md px-4 py-2 bg-white">
+            <div className="text-gray-700 mt-2 flex justify-between border border-gray-200 rounded overflow-hidden px-4 py-2 bg-white">
               <span className="flex-1 font-semibold">{match.team1}</span>
               <span className="flex-1 text-center">{formattedTime}</span>
               <span className="flex-1 text-right font-semibold">
